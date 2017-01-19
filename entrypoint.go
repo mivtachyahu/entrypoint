6304d3ce7bdb7e4913935d4d706649526eea1620
Matthew Brown <matthew@freezeproshop.com>
2017-01-19 12:34:17 +0000

    adding cache dir functionality

--- entrypoint.go
+++ entrypoint.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -88,8 +89,43 @@ func getBucketRegion(bucketName string) *string {
 	return resp.LocationConstraint
 }
 
-func getFile(fileName, bucketName, keyName string) {
-	Trace.Println("getFile Function")
+func getFileSize(fileName string) int64 {
+	file, err := os.Open(fileName)
+	if err != nil {
+		Error.Fatal("Failed to open file", err)
+	}
+	defer file.Close()
+
+	stats, statsErr := file.Stat()
+	if statsErr != nil {
+		Error.Fatal("Failed to stat file", statsErr)
+	}
+	size := stats.Size()
+	return size
+}
+
+func getObjectDetails(bucketName, keyName string) *s3.HeadObjectOutput {
+	Trace.Println("getObjectDetails Function")
+	bucketRegion := getBucketRegion(bucketName)
+	sess, err := session.NewSession(&aws.Config{Region: bucketRegion})
+	if err != nil {
+		Error.Fatal("failed to create session,", err)
+	}
+
+	svc := s3.New(sess)
+	params := &s3.HeadObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(keyName),
+	}
+	resp, err := svc.HeadObject(params)
+
+	if err != nil {
+		Error.Fatal(err.Error())
+	}
+	return resp
+}
+
+func downloadFile(fileName, bucketName, keyName string) {
 	file, err := os.Create(fileName)
 	if err != nil {
 		Error.Fatal("Failed to create file", err)
@@ -111,10 +147,63 @@ func getFile(fileName, bucketName, keyName string) {
 	Info.Println("Downloaded file", file.Name(), numBytes, "bytes")
 }
 
+func copyFile(src, dst string) (int64, error) {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer srcFile.Close()
+
+	srcFileStat, err := srcFile.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	if !srcFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
+}
+
+func getFile(fileName, bucketName, cacheName, keyName string) {
+	Trace.Println("getFile Function")
+	localFile := strings.Join([]string{cacheName, keyName}, "")
+	localPath := strings.Join(strings.Split(localFile, "/")[:len(strings.Split(localFile, "/"))-1], "/")
+	createDir(localPath)
+	if _, err := os.Stat(localFile); os.IsNotExist(err) {
+		Trace.Println("File doesn't exist - creating")
+		downloadFile(localFile, bucketName, keyName)
+		copyFile(localFile, fileName)
+	} else {
+		Trace.Println("File exists - comparing sizes")
+		if *getObjectDetails(bucketName, keyName).ContentLength == getFileSize(fileName) {
+			Trace.Println("Sizes Match - not downloading")
+		} else {
+			downloadFile(localFile, bucketName, keyName)
+			copyFile(localFile, fileName)
+		}
+	}
+}
+
+func createDir(path string) {
+	err := os.MkdirAll(path, 0644)
+	if err != nil {
+		Error.Fatal("Failed to create  dir", err)
+	}
+	Info.Println("Created folder", path)
+}
+
 func main() {
 	bucketPtr := flag.String("bucket", "", "bucket where the file is located")
 	keyPtr := flag.String("key", "", "key within the bucket")
 	destPtr := flag.String("dest", "", "destination to store the file")
+	cachePtr := flag.String("cache", "/var/cache/download", "destination to store the file temporarily")
 	execPtr := flag.String("exec", "", "executable and any arguments to run")
 	verbosePtr := flag.Bool("v", false, "Verbose logging")
 	flag.Parse()
@@ -123,6 +212,7 @@ func main() {
 	} else {
 		LogInit(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	}
+
 	Info.Println("Entrypoint for Docker Containers on AWS")
 	if *bucketPtr == "" || *keyPtr == "" || *destPtr == "" {
 		flag.PrintDefaults()
@@ -130,7 +220,7 @@ func main() {
 	}
 	command := strings.Split(*execPtr, " ")[0]
 	args := strings.Split(*execPtr, " ")[1:]
-	getFile(*destPtr, *bucketPtr, *keyPtr)
+	getFile(*destPtr, *bucketPtr, *cachePtr, *keyPtr)
 	if command != "" {
 		runExec(command, args)
 	}
